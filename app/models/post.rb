2d0d00a505519ed99593af626a2cccdb51ba42a2
Ramy Khorshed <RamyKhorshed@gmail.com>
2017-10-17 10:36:57 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :summary, length: { maximum: 250 }
+  validates :content, length: { minimum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :sensationalist
+
+
+  TITLES = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
+
+  def sensationalist
+    if TITLES.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
